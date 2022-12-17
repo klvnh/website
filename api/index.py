c478f2f6e92d8d2c88b78c4bdca646c8e9d6de63
Yonipy <itz.kqlvin@gmail.com>
2022-12-16 23:06:32 -0500

    eeeee

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from guis.currently_playing import currently_playing
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', currently_playing=currently_playing())
+
+app.run(host='0.0.0.0')
