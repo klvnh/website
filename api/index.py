26252b7d1b09f4567646550ea539f1a1c595c427
Yonipy <itz.kqlvin@gmail.com>
2022-12-16 23:14:11 -0500

    eeee

--- api/index.py
+++ api/index.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
-from api.guis.currently_playing import currently_playing
+import spotipy
+import spotipy.util as util
+import os
 
+username = "klv"
+scope = "user-read-currently-playing"
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+redirect_uri = "https://kvtt.ml/callback"
+token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
+sp = spotipy.Spotify(auth=token)
 app = Flask(__name__)
+
+def currently_playing():
+    song_data = sp.currently_playing()
+    try: 
+        return song_data['item']['album']['artists'][0]['external_urls']['spotify']
+    except:
+        return 'No current playing song'
+
 
 @app.route('/')
 def home():
     return render_template('home.html', currently_playing=currently_playing())
-
-app.run(host='0.0.0.0')
