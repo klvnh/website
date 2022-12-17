08cdb11e0e02705dedbb08c79b9402d2a8dc9914
Yonipy <itz.kqlvin@gmail.com>
2022-12-16 23:39:32 -0500

    eeee

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def currently_playing():
     song_data = sp.currently_playing()
     try: 
-        song_url = song_data["item"]["external_urls"]["spotify"] + "?utm_source=generator"
+        song_url = song_data["item"]["external_urls"]["spotify"]
         return song_url
     except:
         return None
