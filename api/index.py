8efc69e1d719a2faa3dce32257dbf6df1fdcf435
kvtt <75043612+yonipy@users.noreply.github.com>
2022-12-18 11:11:02 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -23,6 +23,6 @@ def currently_playing():
 @app.route('/')
 def home():
     if currently_playing() is None:
-        return "<body style=background-color:#2a2a2a;> No current playing song </body>"
+        return "<body style=background-color:#2a2a2a;> <p> No current playing song</p> </body>"
     else:
         return render_template('home.html', currently_playing=currently_playing())
