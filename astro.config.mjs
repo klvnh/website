8edfa8527707a95d88f4c7cd1b49cf5a33fd65b2
Yonipy <itz.kqlvin@gmail.com>
2023-02-13 17:03:21 -0500

    Please work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
+
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
