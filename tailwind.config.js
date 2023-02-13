11b87c9f51463fcc03bb7e08f5281cf5c41b8463
Yonipy <itz.kqlvin@gmail.com>
2023-02-12 21:19:37 -0500

    REVAMPPPPP

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'mono': "'Ubuntu', sans-serif"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
