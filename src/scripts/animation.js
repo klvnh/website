c3ddc3f67d0cec8fe9e90206a6475d989cc30100
Yonipy <itz.kqlvin@gmail.com>
2023-02-24 16:52:33 -0500

    NEW EASTER EGGS

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,8 +1,15 @@
-const container = document.getElementById("container");
+const intro = document.getElementById("intro");
 
 function animation() {
-    null // Working on this right now...
-
+    // Working on this right now...
+    intro.addEventListener('click', function() {
+        if (intro.innerText == "Hello, Im Kelvin.") {
+            intro.innerText = "Im a junior in HS."
+        } else if (intro.innerText == "Im a junior in HS.") {
+            intro.innerText = "I'm from NYC"
+        } else if (intro.innerText == "I'm from NYC") {
+            intro.innerText = "Hello, Im Kelvin."
+        }})
 }
 
 animation()
